6a53af7293197162b626acad9503a88f41d169f7
gughog <gustavogiovanne3@gmail.com>
2019-07-11 20:15:23 -0300

    education section finished

--- 005-portfolio/script.js
+++ 005-portfolio/script.js
@@ -15,16 +15,34 @@ const data = {
         {
             id: 01,
             institution: 'Federal Institute of Alagoas',
+            institutionWebSite: 'https://www2.ifal.edu.br/',
             course: 'Computer Information Systems',
-            degree: 'Bachelor of Coputional Information Systems',
-            conclusion: '2023'
+            degree: 'Bachelor of Computional Information Systems',
+            conclusion: 'December, 2023'
         },
         {
             id: 02,
             institution: 'Estacio of Alagoas - FAL',
+            institutionWebSite: 'http://portal.estacio.br/unidades/faculdade-est%C3%A1cio-de-alagoas/',
             course: 'Administration Business',
             degree: 'Bachelor of Administration Business',
-            conclusion: '2019'
+            conclusion: 'July, 2019'
         }
+    ],
+    projects: [
+
     ]
 }
+
+
+const education = document.getElementById('education')
+
+education.innerHTML = data.education.map(item =>{
+    return `
+        <h4>${item.degree}</h4>
+        <h5><a href="${item.institutionWebSite}">${item.institution}</a></h5>
+        <h5>${item.course}</h5>
+        <h5>${item.conclusion}</h5>
+        <div class="divisor2-2"></div>
+    `
+}).join('')

eeca6772e66c11182e43eeb42a5373a7cc1dbcdf
gughog <gustavogiovanne3@gmail.com>
2019-07-12 00:35:11 -0300

    added screenshots; projects section finished

--- 005-portfolio/script.js
+++ 005-portfolio/script.js
@@ -3,9 +3,13 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let sidenavs = document.querySelectorAll('.sidenav')
     let floating_btn = document.querySelectorAll('.fixed-action-btn')
+    let icon_tooltips = document.querySelectorAll('.tooltipped')
+    let carousel = document.querySelectorAll('.carousel')
 
     let sidenavi_instances = M.Sidenav.init(sidenavs)
     let floating_btn_instances = M.FloatingActionButton.init(floating_btn);
+    let icon_tooltips_instances = M.Tooltip.init(icon_tooltips)
+    let carousel_instances = M.Carousel.init(carousel, { fullWidth: true, padding: 40})
 })
 
 // Data:
@@ -30,27 +34,79 @@ const data = {
         }
     ],
     skills: [
-        { id: 'html5', class: 'fab fa-html5'},
-        { id: 'css3', class: 'fab fa-css3'},
-        { id: 'js', class: 'fab fa-js'},
-        { id: 'react', class: 'fab fa-react'},
-        { id: 'linux', class: 'fab fa-ubuntu'},
-        { id: 'bootstrap', class: 'fab fa-bootstrap'},
-        { id: 'materialize', class: 'fab fa-google'},
-        { id: 'git', class: 'fab fa-git'},
-        { id: 'npm', class: 'fab fa-npm'},
-        { id: 'yarn', class: 'fab fa-yarn'},
+        { id: 'HTML5', class: 'fab fa-html5'},
+        { id: 'CSS3', class: 'fab fa-css3'},
+        { id: 'Javascript', class: 'fab fa-js'},
+        { id: 'React.js', class: 'fab fa-react'},
+        { id: 'Linux/Ubuntu', class: 'fab fa-ubuntu'},
+        { id: 'Bootstrap CSS', class: 'fab fa-bootstrap'},
+        { id: 'Materialize CSS', class: 'fab fa-google'},
+        { id: 'Git/Github', class: 'fab fa-git'},
+        { id: 'NPM', class: 'fab fa-npm'},
+        { id: 'Yarn', class: 'fab fa-yarn'},
+    ],
+    projects: [
+        {
+            id: 001,
+            link: 'https://github.com/gughog/crud-react-app',
+            name: 'CRUD React Todo App',
+            description: 'A simple To-do app to train my skills in React.js, React Router and Axios.',
+            image: './assets/img/project-pics/react-todo.png'
+        },
+        {
+            id: 002,
+            link: 'https://github.com/gughog/personal-budget-app',
+            name: 'Personal Budget App',
+            description: 'A web application with local data persistent for you keep track of your expenses.',
+            image: './assets/img/project-pics/personal-budget.png'
+        },
+        {
+            id: 003,
+            link: 'https://github.com/gughog/realtime-html-editor',
+            name: 'HTML5 Editor',
+            description: 'A little html5 editor to test tiny pieces of codes and see the result in real time.',
+            image: './assets/img/project-pics/html-editor.png'
+        },
+        {
+            id: 004,
+            link: 'https://github.com/gughog/freeCodeCamp-projects/tree/master/001-tribute-page',
+            name: 'Alice In Chains Tribute Page',
+            description: 'A tribute page for Alice in Chains, as proposed in freeCodeCamp course to build a tribute page.',
+            image: './assets/img/project-pics/aic-tribute.png'
+        },
+        {
+            id: 005,
+            link: 'https://github.com/gughog/freeCodeCamp-projects/tree/master/002-survey-form',
+            name: 'Gaming Survey Form',
+            description: 'A Gaming survey Form, proposed in freeCodeCamp course.',
+            image: './assets/img/project-pics/gaming-form.png'
+        },
+        {
+            id: 006,
+            link: 'https://github.com/gughog/freeCodeCamp-projects/tree/master/003-product-landing-page',
+            name: 'Technobucket Landing Page',
+            description: 'A landing page for the fictional service "Technobucket".',
+            image: './assets/img/project-pics/technobucket.png'
+        },
+        {
+            id: 007,
+            link: 'https://github.com/gughog/freeCodeCamp-projects/tree/master/004-technical-docs',
+            name: 'React Docs',
+            description: 'A mini technical documentation about React, as proposed in freeCodeCamp course.',
+            image: './assets/img/project-pics/react-docs.png'
+        }
     ]
 }
 
 
 const education = document.getElementById('education')
 const skills = document.getElementById('skills')
+const cards = document.getElementById('cards')
 
 education.innerHTML = data.education.map(item =>{
     return `
         <h4>${item.degree}</h4>
-        <h5><a href="${item.institutionWebSite}">${item.institution}</a></h5>
+        <h5><a class="indigo-text" href="${item.institutionWebSite}">${item.institution}</a></h5>
         <h5>${item.course}</h5>
         <h5>${item.conclusion}</h5>
         <div class="divisor2-2"></div>
@@ -59,6 +115,16 @@ education.innerHTML = data.education.map(item =>{
 
 skills.innerHTML = data.skills.map(item =>{
     return `
-        <i class="${item.class} skill-icon"></i>
+        <i class="${item.class} skill-icon tooltipped" data-position="bottom" data-tooltip="${item.id}"></i>
+    `
+}).join('')
+
+cards.innerHTML = data.projects.map(item=>{
+    return `
+    <div id="card" class="carousel-item grey darken-2 white-text" href="#one!">
+        <h2><a href="${item.link}" class=" cyan-text text-accent-3">${item.name} <i class="fas fa-external-link-alt"></i></a></h2>
+        <p class="">${item.description}</p>
+        <img src="${item.image}" alt="">
+    </div>
     `
 }).join('')
